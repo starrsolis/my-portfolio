b3170ed327556af5d8a3b80a668369955d3c96c9
Starr Solis <starr.solis7@gmail.com>
2023-03-06 14:34:59 -0600

    banner

---
+++ src/components/Banner.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/photo.png";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "Software Engineer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+}
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                    <span className="tageline">Welcome to my Portfolio</span>
+                    <h1>{"Hi, I'm Starr Solis "}<span className="wrap">{text}</span></h1>
+                    <p>I am a chronically curious Software Engineer, who is results-driven and people focused. I love to explore innovative solutions and work alongside a team. I am inspired by the ever-changing programming landscape and learning new tools, programming languages, and frameworks to create useful software. As someone who has worn many hats over the years, I look forward to bringing my excitement, optimism and keen problem-solving skills to the tech industry.</p>
+                    <button onClick={() => console.log('connect')}>Let's Connect<ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                    <img src={headerImg} alt="Header Img" className='photo'/>
+                        </Col>
+                </Row>
+            </Container>
+
+        </section>
+    )
+}
