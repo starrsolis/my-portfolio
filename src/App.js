5ae11c00dab6588c442bb5399a654005723de751
Starr Solis <starr.solis7@gmail.com>
2023-03-06 15:00:36 -0600

    Skills

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <div className="App"> 
     < NavBar />
-    < Banner />
+    <Banner />
+    <Skills />
     </div>
   );
 }
