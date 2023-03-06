0370e0ab94cbbf4ed95ae1b3c0c1ef9410297fe2
Starr Solis <starr.solis7@gmail.com>
2023-03-06 16:28:58 -0600

    playing with project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,49 +1,56 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
+import navIcon2 from '../assets/img/nav-icon2.svg';
 
 
 export const Projects = () => {
 
   const projects = [
     {
-      title: "Project",
-      description: "Description",
-      imgUrl: {}, alt:"Image",
+      title: "Travel Agency",
+      description: "React, Javascript ",
+      gitUrl: navIcon2 , 
+      imgUrl: projImg1, alt: "project"
     },
     {
-        title: "Project",
-        description: "Description",
-        imgUrl: {}, alt:"Image",
+        title: "Travel Agency",
+        description: "React, Javascript ",
+        gitUrl:"https://gitlab.com/starrsolis",
+        imgUrl: projImg1, alt: "project"
       },
       {
-        title: "Project",
-        description: "Description",
-        imgUrl: {}, alt:"Image",
+        title: "Travel Agency",
+        description: "React, Javascript ",
+        gitUrl:"https://gitlab.com/starrsolis",
+        imgUrl: projImg1, alt: "project"
       },
   ];
 
   return (
     <section className="project" id="project">
-        <Container>
-            <Row>
-                <Col size={12}>
+      <Container>
+        <Row>
+          <Col size={12}>
+            
+
                 <h2>Projects</h2>
                 <p></p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first"> Full Stack </Nav.Link>
+                      <Nav.Link eventKey="first">Full Stack</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second"> Front-End </Nav.Link>
+                      <Nav.Link eventKey="second">Front-End</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third"> Back-End </Nav.Link>
+                      <Nav.Link eventKey="third">Back-End</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content>
-                  <Tab.Pane eventKey="first">
+                    <Tab.Pane eventKey="first">
                       <Row>
                         {
                           projects.map((project, index) => {
@@ -55,8 +62,8 @@ export const Projects = () => {
                             )
                           })
                         }
-                        </Row>
-                        </Tab.Pane>
+                      </Row>
+                    </Tab.Pane>
                     <Tab.Pane eventKey="section">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
@@ -65,6 +72,8 @@ export const Projects = () => {
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
+              
+            
           </Col>
         </Row>
       </Container>
